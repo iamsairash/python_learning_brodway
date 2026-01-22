3faa337f6a227e4c445587e715d55411f1f13b00
Sairash Chaudhary <sairash0323@gmail.com>
2026-01-22 07:08:36 +0545

    create snake game

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
 print("Hello World")
+"""
+    git status --> hami kun branch ma xam, k k vayo file change
+    git add . --> add all the changes to the staging area, this tells github, which file has been changes you wnat to 
+    add to the next commit.
+    git commit -m "message" --> k kaam gareko message, this make a snapshot of the changes you have made
+
+    git push origin main 
+"""
